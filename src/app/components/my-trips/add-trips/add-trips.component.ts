dc195ae5f460e6f740f43b88374b5e5d50b1a1f1
AshirMohab <amohabir@jhb.dvt.co.za>
2022-04-24 23:03:01 +0200

    fix: :art: fixes to the currency store

--- src/app/components/my-trips/add-trips/add-trips.component.ts
+++ src/app/components/my-trips/add-trips/add-trips.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { CurrencyData } from 'src/app/models/currency';
 import TripsModel from 'src/app/models/tripsModel';
 import User from 'src/app/models/user';
+import { getCurrencyRates } from 'src/app/store/currency/currency.actions';
+import { CurrencyState } from 'src/app/store/currency/currency.reducer';
 import { addUserTrip } from 'src/app/store/trip/trip.actions';
 import { TripState } from 'src/app/store/trip/trip.reducer';
+import * as CurrencySelectors from 'src/app/store/currency/currency.selectors';
 
 @Component({
   selector: 'app-add-trips',
@@ -15,6 +18,8 @@ import { TripState } from 'src/app/store/trip/trip.reducer';
 })
 export class AddTripsComponent implements OnInit {
   addTripForm!: FormGroup;
+  currencyResponse$!: Observable<CurrencyData | null>;
+
   addTripResponse$!: Observable<TripsModel>;
   @Input() currencyRates!: CurrencyData;
   isAdding: boolean = false;
@@ -23,11 +28,16 @@ export class AddTripsComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private tripStore: Store<TripState>
+    private tripStore: Store<TripState>,
+    private currencyStore: Store<CurrencyState>
   ) {}
 
   ngOnInit(): void {
     this.initForm();
+    this.currencyStore.dispatch(getCurrencyRates());
+    this.currencyResponse$ = this.currencyStore.pipe(
+      select(CurrencySelectors.selectCurrrencyRates)
+    );
   }
 
   initForm() {
